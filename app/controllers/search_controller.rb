533988e320bcae407e37ff6e507db50d5cf60573
Sam Devine <samuel.will.devine@gmail.com>
2021-12-17 12:34:03 -0500

    clean up controller slightly

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,9 @@
 class SearchController < ApplicationController
   def index
-    # conn = Faraday.new('https://last-airbender-api.herokuapp.com/')
-    # response = conn.get("api/v1/characters?affiliation=#{params[:nation].sub('_', '+')}&perPage=25")
-    # response2 = conn.get("api/v1/characters?affiliation=#{params[:nation].sub('_', '+')}&perPage=9999")
-    @members = CharacterFacade.first_25_characters(params[:nation])
-    @total_members_count = CharacterFacade.all_characters_count(params[:nation])
+    @members = {
+      nation: params[:nation].sub('_', ' ').titleize,
+      members: CharacterFacade.first_25_characters(params[:nation]),
+      total_count: CharacterFacade.all_characters_count(params[:nation])
+    }
   end
 end
