b0cc9ce42704f8a1c3b174d12f1dab45fc036092
Jacob Yarborough <jayarborough95@gmail.com>
2021-12-17 09:36:57 -0700

    Create get_nation_name method in search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,15 @@
 class SearchController < ApplicationController
   def index 
+    binding.pry
+    @characters = CharacterFacade.get_all_characters(get_nation_name)
+  end 
+
+  private 
+
+  def get_nation_name
+    split_name = params[:nation].split('_')
+    first = split_name[0].to_s 
+    second = split_name[1].to_s 
+    first.capitalize! + '+' second.capitalize!
   end 
 end 
