e72db43d03c6264bfcf96c17e0c1674f1e0c5b7c
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-12-17 14:00:48 -0700

    refactor

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,10 @@
 class SearchController < ApplicationController
   def index
-    @characters = CharactersFacade.characters_search(params[:nation])
-    require "pry"; binding.pry
+    @characters = CharactersFacade.characters_search(
+      params[:nation].gsub("fire_nation", "Fire+Nation")
+                     .gsub("air_nomads", "Air+Nomads")
+                     .gsub("water_tribes", "Water+Tribes")
+                     .gsub("earth_kingdom", "Earth+Kingdom")
+                   )
   end
 end
