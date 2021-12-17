9db43f143a68ebb5b1f4d555b9f1e77fed4d3426
Philip Medlin <phmedlin@gmail.com>
2021-12-17 10:58:09 -0700

    Final commit

--- spec/features/search/index_spec.rb
+++ spec/features/search/index_spec.rb
@@ -8,8 +8,13 @@ RSpec.describe 'Search Page' do
     click_button
 
     expect(page).to have_content("Number of members: 20")
-    expect(page).to have_content("Chan (Fire Nation admiral)")
-    expect(page).to have_content("Circus master")
+    expect(page).to have_content("Nation: Fire nation")
+    expect(page).to have_content("Name: Chan (Fire Nation admiral)")
+    expect(page).to have_content("Allies: Ozai")
+    expect(page).to have_content("Enemies: Earth Kingdom")
+    expect(page).to have_content("Affiliations: Fire Nation Navy")
+
+    expect(page).to have_content("Name: Circus master")
   end
 
 end
