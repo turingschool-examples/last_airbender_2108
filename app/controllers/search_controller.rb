9fb59f265d22684ea0e8307bff62e66455494239
Sam Devine <samuel.will.devine@gmail.com>
2021-12-17 11:47:51 -0500

    build results page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,7 @@
 class SearchController < ApplicationController
   def index
+    conn = Faraday.new('https://last-airbender-api.herokuapp.com/')
+    response = conn.get("api/v1/characters?affiliation=#{params[:nation].sub('_', '+')}")
+    @members = JSON.parse(response.body, symbolize_names: true)
   end
 end
--- spec/features/search/search_spec.rb
+++ spec/features/search/search_spec.rb
@@ -1,12 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe 'Search' do
-  it 'redirects to the search path' do
+  before :each do
     visit root_path
-
-    select 'Fire Nation', from: :nation
+    @nation = 'Fire Nation'
+    select @nation, from: :nation
     click_on 'Search For Members'
+  end
 
+  it 'redirects to the search path' do
     expect(current_path).to eq '/search'
+  end
+
+  it 'displays the total number of people who live in the nation' do
+    expect(page).to have_content "Total #{@nation} members: 100"
+  end
+
+  it 'displays detailed information for the first 25 members' do
+    expect(page).to have_content("Name: ", count: 25)
+    expect(page).to have_content("Allies: ", count: 25)
+    expect(page).to have_content("Enemies: ", count: 25)
+  end
+
+  it 'displays a name, photo, and list of allies, enemies, and affiliations for each member' do
+
   end
 end
