b2ecd2376c491b0d84081d899544a98b6b21e094
Micha Bernhard <mib70@pitt.edu>
2021-12-17 10:06:39 -0700

    Feat: Create member Poro

---
+++ app/poros/member.rb
@@ -0,0 +1,18 @@
+# require './app/facades/nation_facade'
+
+class Member
+  attr_reader :_id,
+              :name,
+              :affiliation,
+              :allies,
+              :enemies,
+              :photoUrl
+  def initialize(data)
+    @_id = data[:_id]
+    @name = data[:name]
+    @affiliation = data[:affiliation]
+    @allies = data[:allies]
+    @enemies = data[:enemies]
+    @photoUrl = data[:photoUrl]
+  end
+end
