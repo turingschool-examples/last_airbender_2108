0d0883a9f22f03d9be7b28cc5c5e5ebb9b764202
Chaz Simons <chaz.simons@gmail.com>
2021-12-17 08:34:53 -0800

    Successfully connect and retrieve nation members

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,11 @@
 class SearchController < ApplicationController
 
   def index
+    nation = params[:nation].gsub("_", "+")
+
+    response = Faraday.get("https://last-airbender-api.herokuapp.com/api/v1/characters?affiliation=#{nation}")
+
+    require "pry"; binding.pry
+    @members = JSON.parse(response.body, symbolize_names: true)
   end
 end
