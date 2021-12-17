1e5d44bd13e4adfe77ebf9a9560a0ad8e143e9fd
Micha Bernhard <mib70@pitt.edu>
2021-12-17 10:15:17 -0700

    Feat: Add conditionals for the other nations

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,15 @@ class SearchController < ApplicationController
     if params[:nation] == 'fire_nation'
       @members = NationFacade.fire_nation_members
       @detailed_list = @members[0..24]
+    elsif params[:nation] == 'air_nomads'
+      @members = NationFacade.air_nomads_members
+      @detailed_list = @members[0..24]
+    elsif params[:nation] == 'earth_kingdom'
+      @members = NationFacade.earth_kingdom_members
+      @detailed_list = @members[0..24]
+    elsif params[:nation] == 'water_tribes'
+      @members = NationFacade.water_tribes_members
+      @detailed_list = @members[0..24]
     end
   end
 end
