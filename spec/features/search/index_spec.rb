ca6d0d9fd6bc25b16b0f410a913d1d614b0a3d36
Micha Bernhard <mib70@pitt.edu>
2021-12-17 10:28:39 -0700

    Refactor: Make it blocks more descriptive

--- spec/features/search/index_spec.rb
+++ spec/features/search/index_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Search Index page' do
-  it 'displays the total number of people that live in the selected nation' do
+  it 'displays the total number of people that live in the Fire Nation' do
     visit root_path
     select 'Fire Nation', from: :nation
     click_button 'Search For Members'
@@ -10,7 +10,7 @@ RSpec.describe 'Search Index page' do
     expect(page).to have_content("97 Members")
   end
 
-  it 'displays detailed information about the first 25 members' do
+  it 'displays detailed information about the first 25 members of the Fire Nation' do
     visit root_path
     select 'Fire Nation', from: :nation
     click_button 'Search For Members'
@@ -26,18 +26,18 @@ RSpec.describe 'Search Index page' do
     expect(page).to have_content("Enemies: Combustion Man")
   end
 
-  it 'displays the total number of people that live in the selected nation' do
+  it 'displays the total number of people that live in the Air Nomads' do
     visit root_path
     select 'Air Nomads', from: :nation
     click_button 'Search For Members'
 
     expect(current_path).to eq(search_path)
-    expect(page).to have_content("97 Members")
+    expect(page).to have_content("15 Members")
   end
 
-  it 'displays detailed information about the first 25 members' do
+  it 'displays detailed information about the first 25 members in the Air Nomads' do
     visit root_path
-    select 'Fire Nation', from: :nation
+    select 'Air Nomads', from: :nation
     click_button 'Search For Members'
 
     expect(page).to have_content("Name: Aang (pilot)")
@@ -49,5 +49,55 @@ RSpec.describe 'Search Index page' do
     expect(page).to have_content("Affiliation: Air Nomads Team Avatar")
     expect(page).to have_content("Allies: Aang")
     expect(page).to have_content("Enemies: Ozai")
+  end
+
+  it 'displays the total number of people that live in the Earth Kingdom' do
+    visit root_path
+    select 'Earth Kingdom', from: :nation
+    click_button 'Search For Members'
+
+    expect(current_path).to eq(search_path)
+    expect(page).to have_content("76 Members")
+  end
+
+  it 'displays detailed information about the first 25 members of the Earch Kingdom' do
+    visit root_path
+    select 'Earth Kingdom', from: :nation
+    click_button 'Search For Members'
+
+    expect(page).to have_content("Name: Arik")
+    expect(page).to have_content("Affiliation: Earth Kingdom Air Force")
+    expect(page).to have_content("Allies: His ")
+    expect(page).to have_content("Enemies: Korra")
+
+    expect(page).to have_content("Name: Haru's mother")
+    expect(page).to have_content("Affiliation: Earth Kingdom")
+    expect(page).to have_content("Allies: Aang ")
+    expect(page).to have_content("Enemies: Fire Nation")
+  end
+
+  it 'displays the total number of people that live in the Water Tribes' do
+    visit root_path
+    select 'Water Tribes', from: :nation
+    click_button 'Search For Members'
+
+    expect(current_path).to eq(search_path)
+    expect(page).to have_content("44 Members")
+  end
+
+  it 'displays detailed information about the first 25 members of the Water Tribes' do
+    visit root_path
+    select 'Water Tribes', from: :nation
+    click_button 'Search For Members'
+
+    expect(page).to have_content("Name: Amon")
+    expect(page).to have_content("Affiliation: Equalists (formerly) Northern Water Tribe (formerly)")
+    expect(page).to have_content("Allies: Tarrlok ")
+    expect(page).to have_content("Enemies: Lin Beifong")
+
+    expect(page).to have_content("Name: Niyok")
+    expect(page).to have_content("Affiliation: Earthen Fire Refinery Water Tribe")
+    expect(page).to have_content("Allies: Nutha ")
+    expect(page).to have_content("Enemies: None")
   end
 end
