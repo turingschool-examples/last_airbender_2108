8877bc0d63f81dd347da096cd37cdf5a38ed571c
Micha Bernhard <mib70@pitt.edu>
2021-12-17 10:06:04 -0700

    Test: Detailed information for first 25 members

--- spec/features/search/index_spec.rb
+++ spec/features/search/index_spec.rb
@@ -7,6 +7,22 @@ RSpec.describe 'Search Index page' do
     click_button 'Search For Members'
 
     expect(current_path).to eq(search_path)
-    expect(page).to have_content("The Fire Nation has 100 members")
+    expect(page).to have_content("97 Members")
+  end
+
+  it 'displays detailed information about the first 25 members' do
+    visit root_path
+    select 'Fire Nation', from: :nation
+    click_button 'Search For Members'
+
+    expect(page).to have_content("Name: Chan (Fire Nation admiral)")
+    expect(page).to have_content("Affiliation: Fire Nation Navy")
+    expect(page).to have_content("Allies: Ozai")
+    expect(page).to have_content("Enemies: Earth Kingdom")
+
+    expect(page).to have_content("Name: Fire Nation train conductor")
+    expect(page).to have_content("Affiliation: Fire Nation Fire Nation train")
+    expect(page).to have_content("Allies: Aang")
+    expect(page).to have_content("Enemies: Combustion Man")
   end
 end
