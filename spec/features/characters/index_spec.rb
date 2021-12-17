26e06dc3654f70d4967a89e652165c9095e6df1a
Lesley Sanders <lesleyasanders@gmail.com>
2021-12-17 12:41:53 -0500

    Update view

--- spec/features/characters/index_spec.rb
+++ spec/features/characters/index_spec.rb
@@ -3,12 +3,11 @@ require 'rails_helper'
 RSpec.describe 'character page' do
   describe 'happy path' do
     it 'shows a list of characters and their data' do
-      visit '/'
-
-      select "Fire Nation", from: :nation
+      visit '/search'
 
       expect(page.status_code).to eq 200
-      expect(page).to have_content("Fire Nation")
+      expect(page).to have_content("Azulon")
+      expect(page).to have_content("High Sage")
     end
   end
 end
