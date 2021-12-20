5043478eeb2e7579a4af9419d575e6d2fec7bd4b
Christina Delpone <cdelpone@gmail.com>
2021-12-20 11:55:16 -0700

    Test service returns all results

--- app/services/airbender_service.rb
+++ app/services/airbender_service.rb
@@ -2,12 +2,9 @@ class AirbenderService
   def self.conn
     Faraday.new(url: 'https://last-airbender-api.herokuapp.com/api/v1/')
   end
-# Faraday.new(url: 'https://last-airbender-api.herokuapp.com/api/v1/characters?perPage=10&page=5')
-# https://last-airbender-api.herokuapp.com/api/v1/characters?perPage=10&page=5
-  def self.search_nation(nation)
-    response = conn.get("characters?affiliation=#{nation}")
+  
+  def self.search_nation(nation)!!
+    response = conn.get("characters?perPage=497&page=1&affiliation=#{nation}")
     data = JSON.parse(response.body, symbolize_names: true)
   end
-
-  #create service call method to return more than defaulted 20
 end
--- spec/services/airbender_service_spec.rb
+++ spec/services/airbender_service_spec.rb
@@ -3,15 +3,16 @@ require 'rails_helper'
 RSpec.describe 'Last Airbender API' do
   it 'returns nation specific data' do
     response = AirbenderService.search_nation("fire+nation")
+    expect(response.count).to eq(97)
 
     expect(response).to be_an Array
     expect(response[0]).to be_a Hash
-    
+
     expect(response[0]).to have_key :_id
     expect(response[0]).to have_key :allies
     expect(response[0]).to have_key :enemies
     expect(response[0]).to have_key :name
     expect(response[0]).to have_key :affiliation
-    # expect(response[0]).to have_key :photoUrl
+    expect(response[1]).to have_key :photoUrl
   end
 end
