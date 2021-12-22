0a894252960df1d5564e074328f9cf1296978ebe
Stephanie Helm <stephaniemhelm@gmail.com>
2021-12-21 18:09:19 -0700

    refactor and debug errors

--- spec/facades/member_facade_spec.rb
+++ spec/facades/member_facade_spec.rb
@@ -1,10 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe MemberFacade do
-  xit 'can get member details' do
-    member = MemberFacade.member_information('Fire Nation')
-
-    expect(member). to be_a Array
-    expect(member.count).to eq(20)
-  end
+  
 end
--- spec/services/member_service_spec.rb
+++ spec/services/member_service_spec.rb
@@ -2,20 +2,28 @@ require 'rails_helper'
 
 RSpec.describe MemberService do
     it 'can get details by affiliation' do
-      response = MemberService.nation_members('Fire Nation')
+
+      service = MemberService.new
+      response = service.nation_members('fire_nation')
 
       expect(response).to be_a Array
       expect(response.count).to eq(20)
 
       first_member = response.first
 
-      expect(first_member).to have_key :name
-      expect(first_member[:name]).to be_a String
+      expect(first_member.name).to be_a String
+      expect(first_member).to have_attributes(name: "Chan (Fire Nation admiral)")
 
-      expect(first_member).to have_key :enemies
-      expect(first_member[:enemies]).to be_a Array
+      expect(first_member.allies).to be_a Array
+      expect(first_member).to have_attributes(allies: ["Ozai"])
 
-      expect(first_member).to have_key :affiliation
-      expect(first_member[:affiliation]).to be_a String
+      expect(first_member.enemies).to be_a Array
+      expect(first_member).to have_attributes(enemies: ["Earth Kingdom"])
+
+      expect(first_member.affiliation).to be_a String
+      expect(first_member).to have_attributes(affiliation: "Fire Nation Navy")
+
+      # expect(first_member.photo).to be_a String
+      # expect(first_member).to have_attributes(photo: "https://vignette.wikia.nocookie.net/avatar/images/a/a5/Circus_master.png/revision/latest?cb=20130706153819")
     end
 end
